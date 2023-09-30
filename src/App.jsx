e8858d4379e7304d466a26f09e1e4551fe91c3b5
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-30 14:23:45 +0700

    Create(Page): Intro.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
+import Intro from './components/pages/intro';
 
 function App() {
   return (
-    <>
-
-    </>
+    <section
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        placeContent: 'center',
+        placeItems: 'center',
+      }}
+    >
+      <Intro />
+    </section>
   )
 }
 
