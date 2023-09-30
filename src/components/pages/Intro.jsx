6d05b238f0ec59dd85975dc3e11faf5442bc141f
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-30 15:50:25 +0700

    Refactor(Pages): remove unused and refactor codebase in [Abstraction, Intro]

--- src/components/pages/Intro.jsx
+++ src/components/pages/Intro.jsx
@@ -1,6 +1,6 @@
 import Text from '../partials/Text';
 import Button from '../partials/Button';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
